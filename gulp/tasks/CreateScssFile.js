9b09054dd5b99fffcb155a5aa2173718872f0f00
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2023-02-17 12:09:52 +0000

    fix compiler for scss

--- gulp/tasks/CreateScssFile.js
+++ gulp/tasks/CreateScssFile.js
@@ -3,21 +3,21 @@ const fs = require('fs');
 const getSectionIndexText = require('./CreateScss/SectionIndex');
 const getPartialsList = require('./CreateScss/GetPartialsList');
 
-const envType = {'development':'dev', 'production':'prod'}
+const envType = { 'development': 'dev', 'production': 'prod' }
 let env = '';
 
-function setDevEnvironment (cb) {
+function setDevEnvironment(cb) {
 	env = envType.development;
 	createScssFile(cb);
 }
 
-function setProdEnvironment (cb) {
+function setProdEnvironment(cb) {
 	env = envType.production;
 	createScssFile(cb);
 }
 
 // Create a file note to give important context!
-function getNotesText () {
+function getNotesText() {
 	let notes = '';
 
 	notes += ""
@@ -37,23 +37,16 @@ function getNotesText () {
 	notes += `└── './gulp/ProjectSpecs/ScssStructure/#All.js' \n`;
 	notes += `\n`;
 	notes += `PS: This comment will not be visible at the compiled version!\n`;
-	notes +=	"=============================================================================== */\n";
+	notes += "=============================================================================== */\n";
 
 	return notes;
 }
 
 // Method used to Create SCSS file structure dynamically
 function createScssFile(cb) {
-	// Store the Section Index generated text
-	const sectionIndexText = env === envType.production ? getSectionIndexText.textProd() : getSectionIndexText.textDev();
-	// Store the Partials List generated text
-	const partialsText = env === envType.production ? getPartialsList.textProd() : getPartialsList.textDev();
 
-	// Combine text to create the hole file
-	const newScssText = `${getNotesText()}\n${sectionIndexText}\n${partialsText}`;
-
-    // Update the SCSS file with the generated code!
-    fs.writeFileSync(`./src/scss/OutSystemsUI.scss`, newScssText, 'utf8');
+	// Update the SCSS file with the generated code!
+	fs.writeFileSync(`./src/scss/OutSystemsUI.scss`, '', 'utf8');
 
 	cb();
 }
--- src/scripts/test.ts
+++ src/scripts/test.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace MyOSUI.test {
     export class MyTest {
-    constructor() {
-        console.log('Hello');
+        constructor() {
+            console.log('Hello');
         }
 
     }
